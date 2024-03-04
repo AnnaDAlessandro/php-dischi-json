0a4bd27e46f9232ea3688fa88c2bae8d5fd17d79
Anna <annadalex@icloud.com>
2024-03-04 20:26:39 +0100

    creazione header e pagina di stile

---
+++ src/components/header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="container">
+        <figure>
+            <img class="img-fluid my-2" width="80" height="80"  src="https://cdn.iconscout.com/icon/free/png-512/free-spotify-1682937-1440796.png?f=webp&w=256" alt="">
+        </figure>
+
+    </div>
+</header>
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+console.log('ciao');
+
 const { createApp } = Vue
 
 createApp({
