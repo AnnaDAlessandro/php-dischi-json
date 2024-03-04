dde6858c2ebfb4f3f6041ff9a72063681b95f46b
Anna <annadalex@icloud.com>
2024-03-04 21:08:00 +0100

    creazione card dinamica

--- src/components/main.php
+++ src/components/main.php
@@ -5,12 +5,12 @@
         v-for="(element,index) in dischi"
         :key="index"
         class="col-4 mb-4">
-         <div class="card" id="card-main" style="width: 18rem;">
-          <img src="..." alt="...">
+         <div class="card h-100" id="card-main" style="width: 18rem;">
+          <img :src="element.poster" alt="...">
            <div class="card-body text-center">
-           <h3 class="card-title">fdvf</h3>
-           <span class="card-text"></span>
-           <h4 class="card-text"></h4>
+           <h4 class="card-title">{{element.title}}</h4>
+           <span class="card-text">{{element.author}}</span>
+           <h5 class="card-text my-2">{{element.year}}</h5>
           
            </div>
          </div>
